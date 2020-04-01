944f3d09d7c5eca6859758b2af31b2eedf0117dc
Anton_Gonzo <andreyevanton@mail.ru>
2020-04-02 02:41:05 +0300

    basic sections

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,131 @@
 <template>
   <div>
     <nuxt/>
+    <footer class="footer">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <h3 class="footer_title">О компании</h3>
+            <p class="footer_paragraph_accent">Наименование</p>
+            <p class="footer_paragraph">ООО «Прогресс Агро»</p>
+            <p class="footer_paragraph_accent">Директор</p>
+            <p class="footer_paragraph">Курталидис Михаил Сергеевич</p>
+          </div>
+          <div class="column">
+            <h3 class="footer_title">Контакты</h3>
+            <p class="footer_paragraph_accent">Адрес</p>
+            <p class="footer_paragraph">117279, Москва, ул. Миклухо-Маклая, 34 Э0
+              пом. IV к37 оф 265</p>
+            <p class="footer_paragraph_accent">Телефон</p>
+            <p class="footer_paragraph">+7 916 268-81-87</p>
+            <p class="footer_paragraph_accent">E-mail</p>
+            <p class="footer_paragraph">info@progress-agro.com.ru</p>
+          </div>
+          <div class="column">
+            <h3 class="footer_title">Реквизиты</h3>
+            <div class="footer__block">
+              <div class="footer__block_accent">ИНН/КПП</div>
+              <div class="footer__block_paragraph">7728483081/ 772801001</div>
+            </div>
+            <div class="footer__block">
+              <div class="footer__block_accent">ОГРН</div>
+              <div class="footer__block_paragraph">1197746497629</div>
+            </div>
+            <div class="footer__block">
+              <div class="footer__block_accent">Расчётный счет</div>
+              <div class="footer__block_paragraph">40702810702730003599</div>
+            </div>
+            <div class="footer__block">
+              <div class="footer__block_accent">Корреспондентский счет</div>
+              <div class="footer__block_paragraph">30101810200000000593</div>
+            </div>
+            <div class="footer__block">
+              <div class="footer__block_accent">БИК банка</div>
+              <div class="footer__block_paragraph">044525593</div>
+            </div>
+            <div class="footer__block">
+              <div class="footer__block_accent">Банк</div>
+              <div class="footer__block_paragraph">АО "АЛЬФА-БАНК"</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+  html {
+    font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+  *, *:before, *:after {
+    box-sizing: border-box;
+    margin: 0;
+  }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+  .button--green {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #3b8070;
+    color: #3b8070;
+    text-decoration: none;
+    padding: 10px 30px;
+  }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
+  .button--green:hover {
+    color: #fff;
+    background-color: #3b8070;
+  }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
+  .button--grey {
+    display: inline-block;
+    border-radius: 4px;
+    border: 1px solid #35495e;
+    color: #35495e;
+    text-decoration: none;
+    padding: 10px 30px;
+    margin-left: 15px;
+  }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+  .button--grey:hover {
+    color: #fff;
+    background-color: #35495e;
+  }
+
+  .footer_title{
+    color: #363636;
+    font-size: 1.25rem;
+    font-weight: 600;
+    margin-bottom: 1rem;
+  }
+
+  .footer_paragraph_accent{
+    color: #363636;
+    font-size: 1em;
+    font-weight: 600;
+    margin-bottom: .25rem;
+    margin-top: .5rem;
+  }
+  .footer__block{
+    display: flex;
+    margin-bottom: .25rem;
+    margin-top: .5rem;
+  }
+  .footer__block_accent{
+    color: #363636;
+    font-size: 1em;
+    font-weight: 600;
+  }
+  .footer__block_paragraph{
+    margin-left: .5rem;
+  }
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,227 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        progress_agro
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <div>
+    <div class="nav_wrap">
+
+
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="#">
+            <img src="https://i.pinimg.com/originals/af/f5/78/aff5786664d1ef5b5384f99ee3e137b9.png" class="nav_logo">
+          </a>
+
+          <a role="button" class="navbar-burger burger is-active" aria-label="menu" aria-expanded="false"
+             data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu is-active">
+          <div class="navbar-end">
+            <a class="navbar-item">
+              Главная
+            </a>
+
+            <a class="navbar-item">
+              О компании
+            </a>
+
+            <a class="navbar-item">
+              Контакты
+            </a>
+          </div>
+        </div>
+      </nav>
+
     </div>
-  </section>
+
+    <b-carousel :autoplay="autoPlay">
+      <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
+        <section :class="`hero is-medium is-${carousel.color}`">
+          <span class="image">
+              <img :src="carousel.src">
+          </span>
+        </section>
+      </b-carousel-item>
+    </b-carousel>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-half">
+            <div class="columns">
+              <div class="column is-one-third">
+                <img
+                    src="https://images11.popmeh.ru/upload/img_cache/360/360dca32418be98bfc1013c4fdcd0125_fitted_800x3000.jpg"
+                    width="100%">
+              </div>
+              <div class="column">Хорошие грузовики</div>
+            </div>
+          </div>
+          <div class="column is-half">
+            <div class="columns">
+              <div class="column is-one-third">
+                <img
+                    src="https://images11.popmeh.ru/upload/img_cache/360/360dca32418be98bfc1013c4fdcd0125_fitted_800x3000.jpg"
+                    width="100%">
+              </div>
+              <div class="column">Хорошие грузовики</div>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column is-half">
+            <div class="columns">
+              <div class="column is-one-third">
+                <img
+                    src="https://images11.popmeh.ru/upload/img_cache/360/360dca32418be98bfc1013c4fdcd0125_fitted_800x3000.jpg"
+                    width="100%">
+              </div>
+              <div class="column">Хорошие грузовики</div>
+            </div>
+          </div>
+          <div class="column is-half">
+            <div class="columns">
+              <div class="column is-one-third">
+                <img
+                    src="https://images11.popmeh.ru/upload/img_cache/360/360dca32418be98bfc1013c4fdcd0125_fitted_800x3000.jpg"
+                    width="100%">
+              </div>
+              <div class="column">Хорошие грузовики</div>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column is-half">
+            <div class="columns">
+              <div class="column is-one-third">
+                <img
+                    src="https://images11.popmeh.ru/upload/img_cache/360/360dca32418be98bfc1013c4fdcd0125_fitted_800x3000.jpg"
+                    width="100%">
+              </div>
+              <div class="column">Хорошие грузовики</div>
+            </div>
+          </div>
+          <div class="column is-half">
+            <div class="columns">
+              <div class="column is-one-third">
+                <img
+                    src="https://images11.popmeh.ru/upload/img_cache/360/360dca32418be98bfc1013c4fdcd0125_fitted_800x3000.jpg"
+                    width="100%">
+              </div>
+              <div class="column">Хорошие грузовики</div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-one-fifth">
+            <img src="../assets/images/slide1.jpg">
+          </div>
+          <div class="column">28 доставлеенных грузов из 100!</div>
+        </div>
+        <div class="columns">
+          <div class="column is-one-fifth">
+            <img src="../assets/images/slide2.jpg">
+          </div>
+          <div class="column">Сытые водители!</div>
+        </div>
+        <div class="columns">
+          <div class="column is-one-fifth">
+            <img src="../assets/images/slide3.jpg">
+          </div>
+          <div class="column">Купленные полицейские на всех постах!</div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section feedback">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <h2 class="title">
+              Связаться с нами
+            </h2>
+          </div>
+          <div class="column">
+            <template>
+              <section>
+                <b-field label="Имя">
+                  <b-input v-model="name"></b-input>
+                </b-field>
+
+                <b-field label="E-mail"
+                         type="is-danger"
+                         message="This email is invalid">
+                  <b-input type="email"
+                           value="john@"
+                           maxlength="30">
+                  </b-input>
+                </b-field>
+
+                <b-field label="Сообщение">
+                  <b-input maxlength="200" type="textarea"></b-input>
+                </b-field>
+              </section>
+            </template>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+
+
+  </div>
+
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
+ export default {
+  data () {
+   return {
+    autoPlay: false,
+    carousels: [
+     {text: 'Slide 1', src: '../assets/images/slide1.jpg', color: 'primary'},
+     {text: 'Slide 2', src: '../assets/images/slide2.jpg', color: 'info'},
+     {text: 'Slide 3', src: '../assets/images/slide3.jpg', color: 'success'}
+    ]
+   }
   }
-}
+
+ }
 </script>
+<style lang="scss">
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  $carousel-min-height: '400px';
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  img {
+    display: block;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .nav_wrap {
+    padding: 1rem 1.5rem;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .nav_logo {
+    height: 50px;
+  }
+
+  .title {
+    color: #363636;
+    font-size: 2rem;
+    font-weight: 600;
+    line-height: 1.125;
+  }
+
+  .feedback {
+    background-color: #efefef;
+  }
+
 </style>
-
